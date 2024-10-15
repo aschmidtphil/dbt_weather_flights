eb6de9e686dd7d731d8d27f7e613e8cd3da7f89e
aschmidtphil <aschmidtphil@gmail.com>
2024-10-15 15:56:05 +0200

    added route stats

---
+++ models/mart/mart_route_stats.sql
@@ -0,0 +1,64 @@
+WITH routes AS 
+(	SELECT 
+        origin,                       
+        dest, 
+      --  flight_date,
+      --  dep_time,
+        COUNT(*) AS total_flights,    
+        COUNT(DISTINCT tail_number) AS unique_airplanes, 
+        COUNT(DISTINCT airline) AS unique_airlines,      
+        ROUND(AVG(actual_elapsed_time),2) AS avg_elapsed_time,    
+        ROUND(AVG(arr_delay),2) AS avg_arrival_delay,             
+        MAX(arr_delay) AS max_delay,                     
+        MIN(arr_delay) AS min_delay,                    
+        SUM(cancelled) AS total_cancelled,              
+        SUM(diverted) AS total_diverted                 
+    FROM {{ref('prep_flights')}}
+    GROUP BY origin, dest --,flight_date, dep_time
+), 
+locations AS
+(	SELECT
+		origin,
+		dest,
+		tail_number,
+		airline,
+		actual_elapsed_time,
+		arr_delay,
+		cancelled,
+		diverted,
+		origin_airport.city AS origin_city,
+		origin_airport.country AS origin_country,
+		origin_airport.name AS origin_name,
+		dest_airport.city AS dest_city,
+		dest_airport.country AS dest_country,
+		dest_airport.name AS dest_name
+	FROM {{ref('prep_flights')}}
+	LEFT JOIN {{ref('prep_airports')}} AS origin_airport      -- Alias for the origin airport
+    ON origin_airport.faa = prep_flights.origin
+	LEFT JOIN {{ref('prep_flights')}} AS dest_airport        -- Alias for the destination airport
+    ON dest_airport.faa = prep_flights.dest
+	)
+SELECT 	--flight_date,
+        --dep_time,
+		locations.origin,
+		origin_city,
+		origin_country,
+		origin_name,
+		locations.dest,
+		dest_city,
+		dest_country,
+		dest_name,
+		total_flights,    
+        unique_airplanes, 
+        unique_airlines,      
+        avg_elapsed_time,    
+        avg_arrival_delay,             
+        max_delay,                     
+        min_delay,                    
+        total_cancelled,              
+        total_diverted
+FROM locations 
+JOIN routes
+ON locations.origin = routes.origin
+AND locations.dest = routes.dest
+ORDER BY routes.origin, routes.dest
